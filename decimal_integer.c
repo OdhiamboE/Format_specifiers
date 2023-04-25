aded9bb76083944824261366d1b19d479a2f59b5
OdhiamboE <evanosoft@gmail.com>
2023-04-25 05:20:52 -0700

    format specifiers

---
+++ decimal_integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * Format specifier to print integer values
+ * @printf prints the integer values
+ *
+ * Returns 0
+ */
+
+int main(void)
+{
+	int s = 123;
+	int x = 45;
+	int j = 2;
+	printf("s is %d and x is %d and j is %d\n",s,x,j);
+	{
+		return (0);
+	}
+}
